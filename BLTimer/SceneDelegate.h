a22f53ae5f42da1f9e855b50b95a987efa83057a
freefa <lubiao86@126.com>
2020-02-24 18:47:20 +0800

    Initial Commit

---
+++ BLTimer/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  BLTimer
+//
+//  Created by Louis.B on 2020/2/24.
+//  Copyright © 2020 Louis.B. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ BLTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BLTimer
+//
+//  Created by Louis.B on 2020/2/24.
+//  Copyright © 2020 Louis.B. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
